c717667ee5594da80cdb078d26c16c2d6146ec81
Hasratdeep <hasratdeepkaur383@gmail.com>
2025-09-16 15:49:20 -0500

    performed the tests for portfolioperformance.

--- src/portfolio/portfolioPerformance.ts
+++ src/portfolio/portfolioPerformance.ts
@@ -1,43 +1,43 @@
-export interface PortfolopPerformance {
-    initialInvestment: number;
-    currentValue: number;
-    profitOrLoss: number;
-    percentageChange: number;
-    performanceSUmmary: String;
-}
+    export interface PortfolopPerformance {
+        initialInvestment: number;
+        currentValue: number;
+        profitOrLoss: number;
+        percentageChange: number;
+        performanceSUmmary: String;
+    }
 
-// The function to calculatePortfoliooPerformance //
-    export function calculatePortfolioPerformance(): any {
-    let initialInvestment = 10000;
-    let currentValue = 12000;
+    // The function to calculatePortfoliooPerformance //
+        export function calculatePortfolioPerformance(): any {
+        let initialInvestment =  10000;
+        let currentValue = 12000;
 
-    const profitOrLoss = currentValue - initialInvestment;
-    const percentageChange = (profitOrLoss / initialInvestment) * 100;
+        const profitOrLoss = currentValue - initialInvestment;
+        const percentageChange = (profitOrLoss / initialInvestment) * 100;
 
-// performance summary criteria//
-  let performanceSummary: string;
-  switch(true) {
-    case percentageChange > 20:
-        return performanceSummary = `The portfolio has gained moderately with a profit of $${profitOrLoss}.`;
-    case percentageChange > 10:
-        return performanceSummary = `The portfolio has gained slightly with a profit of $${profitOrLoss}.`;
-    case percentageChange > 0:
-        return performanceSummary = `The portfolio has gained no change with  $${profitOrLoss}.`;
-    case percentageChange > -0.1:
-        return performanceSummary = `The portfolio has lost slightly with a loss of $${profitOrLoss}.`;
-    case percentageChange > -10:
-        return performanceSummary = `The portfolio has lost moderately with a loss of $${profitOrLoss}.`;
-    case percentageChange > -20:
-        return performanceSummary = `The portfolio has lost significantly with a loss of $${profitOrLoss}.`;
-    default:
-        performanceSummary = `The portfolio has performed poorly.`;
-  }
-   
-    return {
-        initialInvestment,
-        currentValue,
-        profitOrLoss,
-        percentageChange,
-        performanceSummary,
-    };
-}
+    // performance summary criteria//
+    let performanceSummary: string;
+        switch(true) {
+            case percentageChange > 20:
+                return performanceSummary = `The portfolio has gained moderately with a profit of $${profitOrLoss}.`;
+            case percentageChange > 10:
+                return performanceSummary = `The portfolio has gained slightly with a profit of $${profitOrLoss}.`;
+            case percentageChange > 0:
+                return performanceSummary = `The portfolio has gained no change with  $${profitOrLoss}.`;
+            case percentageChange > -0.1:
+                return performanceSummary = `The portfolio has lost slightly with a loss of $${profitOrLoss}.`;
+            case percentageChange > -10:
+                return performanceSummary = `The portfolio has lost moderately with a loss of $${profitOrLoss}.`;
+            case percentageChange > -20:
+                return performanceSummary = `The portfolio has lost significantly with a loss of $${profitOrLoss}.`;
+            default:
+                return`The portfolio has performed poorly.`;
+        };
+        
+            return {
+                initialInvestment,
+                currentValue,
+                profitOrLoss,
+                percentageChange,
+                performanceSummary,
+            };
+        }
---
+++ test/portfolioPerformance.test.ts
@@ -0,0 +1,45 @@
+    import { calculatePortfolioPerformance, PortfolopPerformance } from  "../src/portfolio/portfolioPerformance";
+    describe("calculatePortfolioPerformance", () => {
+        it("should correctly identify the profits", () => {
+            //ARRANGE//
+            const initialInvestment = 10000;
+            const currentValue = 12000;
+
+            //ACT//
+           const result = calculatePortfolioPerformance();
+
+            //ASSERT//
+            expect(result).not.toBeNull();
+            expect(result.profitOrLoss).toBe(2000);
+            expect(result.performanceSUmmary).toBe(20);
+            expect(result.performanceSUmmary).toContain("Gained Significantly");
+        })
+
+        it("should correctly identify no change", () => {
+            //ARRANGE//
+            const initialInvestment = 10000;
+            const currentValue = 10000;
+
+            //ACT//
+            const result = calculatePortfolioPerformance();
+
+            //ASSERT//
+            expect(result.profitOrLoss).toBe(0);
+            expect(result.percentageChange).toBe(0);
+            expect(result.performanceSUmmary).toContain("no change");
+        })
+
+        it("should correctly identify the loss", () => {
+            //ARRANGE//
+            const initialInvestment = 10000;
+            const currentValue = 8000;
+
+            //ACT//
+            const result = calculatePortfolioPerformance();
+
+            //ASSERT//
+            expect(result.profitOrLoss).toBe(-2000);
+            expect(result.percentageChange).toBe(-20);
+            expect(result.performanceSUmmary).toContain("loss slightly");
+        });
+    })
